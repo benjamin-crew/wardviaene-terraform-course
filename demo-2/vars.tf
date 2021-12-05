257ced36a31e05526bcebc95ab5060a70b875815
Benjamin Crew <benncrew94@gmail.com>
2021-12-05 11:45:07 +0000

    Demo 2 complete.

--- demo-2/vars.tf
+++ demo-2/vars.tf
@@ -11,18 +11,18 @@ variable "AWS_REGION" {
 variable "AMIS" {
   type = map(string)
   default = {
-    us-east-1 = "ami-13be557e"
+    us-east-1 = "ami-04505e74c0741db8d"
     us-west-2 = "ami-06b94666"
     eu-west-1 = "ami-844e0bf7"
   }
 }
 
 variable "PATH_TO_PRIVATE_KEY" {
-  default = "mykey"
+  default = "demo2-mykey"
 }
 
 variable "PATH_TO_PUBLIC_KEY" {
-  default = "mykey.pub"
+  default = "demo2-mykey.pub"
 }
 
 variable "INSTANCE_USERNAME" {
